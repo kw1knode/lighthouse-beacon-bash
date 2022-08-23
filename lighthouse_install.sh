3b0a93dc1cf7da2c7ff41b7a2455cb635b917dbd
kw1k <vpetoscia@gmail.com>
2022-08-22 18:49:00 -0700

    Create lighthouse_install.sh

---
+++ lighthouse_install.sh
@@ -0,0 +1,35 @@
+sudo apt-get update -y && sudo apt-get upgrade -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+sudo apt install -y git gcc g++ make cmake pkg-config libssl-dev
+sudo apt update
+
+git clone https://github.com/sigp/lighthouse.git
+cd lighthouse
+make
+sudo cp /$HOME/.cargo/bin/lighthouse /usr/local/bin
+sudo useradd --no-create-home --shell /bin/false lighthousebeacon
+sudo mkdir -p /var/lib/lighthouse/beacon-node
+sudo chown -R lighthousebeacon:lighthousebeacon /var/lib/lighthouse/beacon-node
+
+
+echo "[Unit]
+Description=Lighthouse Beacon Node
+After=network.target
+Wants=network.target
+
+[Service]
+User=lighthousebeacon
+Group=lighthousebeacon
+Type=simple
+Restart=always
+RestartSec=5
+ExecStart=/usr/local/bin/lighthouse  beacon_node --checkpoint-sync-url https://<PROJECT-ID>:<PROJECT-SECRET>@eth2-beacon-mainnet.infura.io --datadir /var/lib/lighthouse/beacon-node --network mainnet --metrics --execution-endpoint http://localhost:8545 --execution-jwt /data/erigon/datadir/jwt.hex
+
+[Install]
+WantedBy=default.target" >> /etc/systemd/system/lighthousebeacon.service
+
+sudo systemctl daemon-reload
+sudo systemctl start lighthousebeacon
+
+
+
